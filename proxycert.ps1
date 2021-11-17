121839e793f450d53727705a357d6e74a220f047
yz4898 <14483330+yz4898@users.noreply.github.com>
2021-11-17 11:10:39 -0600

    Update proxycert.ps1

--- proxycert.ps1
+++ proxycert.ps1
@@ -171,29 +171,3 @@ if ($ans -ieq 'Y') {
 
 cd $env:USERPROFILE\Documents\ado_repo
 Write-Host -ForegroundColor Cyan "Setup completed!"
-
-
-# # Setup new ADO repo activities
-# az login
-# $subscriptionID = Read-Host -Prompt 'What is your subscription ID (example xxxx-xxxx-xxxx-xxxx): '
-# az account set -s $subscriptionID
-# az account show
-# Write-Host -ForegroundColor Green "Please verify for the correct subscription "
-# Read-Host "Press any [ENTER] to continue..."
-
-# Write-Host -ForegroundColor Cyan "Create $env:USERPROFILE\Documents\ado_repo if it doesn't exist"
-# if ((Test-Path -Path $env:USERPROFILE\Documents\ado_repo) -ne $true) {
-#     New-Item -Path $env:USERPROFILE\Documents\ado_repo -ItemType Directory
-# }
-
-# $regionName = Read-Host -Prompt 'Which region do you want to deploy to (example westus2): '
-# $adoOrgUrl = Read-Host -Prompt 'What is your ADO Org URL (example https://dev.azure.com/ACC-Azure-06): '
-# $adoProjectName = Read-Host -Prompt 'What is your ADO project name (example 25899-vSPAM): '
-
-# $gitObj = az repos create --name "f5-$regionName" --org "$adoOrgUrl" --project "$adoProjectName" | ConvertFrom-Json
-
-# # git clone $gitObj.remoteUrl
-# cd $env:USERPROFILE\Documents\ado_repo\f5-$regionName
-# # copy source\.README.md $env:USERPROFILE\Documents\ado_repo\f5-$regionName
-# # copy source\.gitignore $env:USERPROFILE\Documents\ado_repo\f5-$regionName
-# # copy -rf srouce\att_mvm\* $env:USERPROFILE\Documents\ado_repo\f5-$regionName
